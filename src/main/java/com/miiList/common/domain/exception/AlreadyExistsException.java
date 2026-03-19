ecf1037a7b862c05f99d482425a8328f5702b710
unknown <rafael.castro.pulido@experis-spain.com>
2026-03-19 09:48:06 +0100

    Common library project

---
+++ src/main/java/com/miiList/common/domain/exception/AlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.miiList.common.domain.exception;
+
+
+public class AlreadyExistsException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
